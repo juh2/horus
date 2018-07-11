5b260ca64beb14ce923f90b5a8f09abe9780cd6b
Jan Ulrich Hasecke <jan.ulrich@hasecke.com>
2018-07-11 10:06:03 +0200

    i18n mit Bildern

--- conf.py
+++ conf.py
@@ -73,7 +73,7 @@ release = '1.0'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = 'de'
-
+locale_dirs = ['locale/']   # path is example but recommended.
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #today = ''
